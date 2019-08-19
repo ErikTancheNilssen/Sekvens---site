464fff2085686a1833d0b884faed459e811ee32a
Mario Michelli <mario.michelli@etn.no>
2019-08-19 09:29:29 +0200

    update footers

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,56 +1,48 @@
 /*global Intercom */
 import React from "react";
 import { Heading, Flex, Text } from "rebass";
+import { Grid, Column } from "./Grid.js";
 import styled from "styled-components";
 import Button from "./Button.js";
 
 const LineBreaker = styled(Text)`
   max-width: 350px;
+  margin: auto;
 `;
 
-const PageBlock = styled(Flex)`
+const TextBlock = styled(Column)`
   text-align: center;
 `;
 
-const TextBlock = styled(Flex)`
-  max-width: 800px;
-`;
-
 const Hero = ({ title, introduction, items, success, action }) => (
-  <Flex flexDirection={["column-reverse", "column-reverse", "row", "row"]}>
-    <PageBlock justifyContent="center" alignItems="center" width={1}>
-      <TextBlock
-        flexDirection="column"
-        textAlign="center"
-        justifyContent="center"
-        alignItems="center"
-        bg="p4"
-        p={6}
-        my={[6]}
-        mx={[3, 3, 3, 6]}
+  <Grid>
+    <TextBlock
+      my={4}
+      p="5"
+      gridColumn={["1/span 4", "1/span 4", "3/span 8"]}
+      bg="p4"
+    >
+      <Heading mb={3} fontSize={[4, 4, 5]}>
+        {title}
+      </Heading>
+
+      {!!introduction && <LineBreaker>{introduction}</LineBreaker>}
+
+      <Button
+        onClick={event => {
+          if (Intercom) {
+            Intercom("show");
+            event.preventDefault();
+          }
+        }}
+        mt={4}
+        as="a"
+        href={success}
       >
-        <Heading mb={3} fontSize={[4, 4, 5]}>
-          {title}
-        </Heading>
-
-        {!!introduction && <LineBreaker>{introduction}</LineBreaker>}
-
-        <Button
-          onClick={event => {
-            if (Intercom) {
-              Intercom("show");
-              event.preventDefault();
-            }
-          }}
-          mt={4}
-          as="a"
-          href={success}
-        >
-          {action}
-        </Button>
-      </TextBlock>
-    </PageBlock>
-  </Flex>
+        {action}
+      </Button>
+    </TextBlock>
+  </Grid>
 );
 
 export default Hero;
